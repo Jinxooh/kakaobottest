e99ea5264bc38ef477d51254a992dd3f0cab5867
황진수-PC <cjswp122@gmail.com>
2018-01-18 13:32:06 +0900

    add routes init

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
+import routes from './routes';
+
 dotenv.config();
 const app = express();
 
 app.set('port', process.env.PORT || 7000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use('/', routes);
+app.use((err, req, res) => {
+  res.status(500).json({ failure: err });
+});
 
 app.listen(app.get('port'), () => console.log('the server is ruunning on ', app.get('port')));
