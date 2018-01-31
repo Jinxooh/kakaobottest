1b2850cdd0655d74da1c9987cf6b754b6803584d
황진수-PC <cjswp122@gmail.com>
2018-01-31 09:43:10 +0900

    making star service

--- src/service/starService.js
+++ src/service/starService.js
@@ -1,13 +1,37 @@
 import Star from 'database/models/Star';
+import messageTypes from 'service/messageTypes';
+
+const MESSAGE = 'onlyMessage';
+const URL_BTN = 'urlButton';
+const PHOTO = 'photo';
+
+const messageType = (type) => {
+  const { startId, question, answer, msgType, urlPhoto, urlButton } = type;
+
+  if (msgType === MESSAGE) {
+    return messageTypes.textMessage(question, answer || null);
+  }
+  // if (msgType === URL_BTN) {
+  //   return messageTypes.urlButtonMessage;
+  // }
+  // if (msgType === PHOTO) {
+  //   return messageTypes.photoMessage;
+  // }
+  return messageTypes.textMessage;
+};
 
 const star = (() => {
   return {
-    service: (user, content, callback) => {
+    service: async (user, content, callback) => {
       console.log('content, ', content);
-      console.log('callback, ', callback);
-    },
-    service2: () => {
-      console.log('tarot service');
+      // console.log('callback, ', callback);
+      const { step } = user;
+      const star = await Star.findStarId(step);
+      // const { startId, question, answer, msgType } = star;
+      // console.log(user);
+      // console.log(star);
+      // console.log(messageType(msgType));
+      callback(messageType(star));
     },
   };
 })();
