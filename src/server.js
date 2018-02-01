942687da7836023f2dc9c06f0009357c51347efc
황진수-PC <cjswp122@gmail.com>
2018-02-01 10:10:48 +0900

    https 적용 준비중

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+
+import http from 'http';
+import https from 'https';
 
 import routes from './routes';
+
+const httpPort = 80;
+const httpsPort = 443;
 
 export default class Server {
   constructor() {
@@ -12,6 +20,7 @@ export default class Server {
 
   middleware() {
     const { app } = this;
+    // app.use(express.static('public'));
     app.use(morgan('dev'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
@@ -24,6 +33,18 @@ export default class Server {
   listen(port) {
     const { app } = this;
     app.listen(port);
+    const file = fs.readFileSync(path.join('/Users/hwangjinsoo-pc/Documents/MyWorks/yarn.lock'));
+    console.log(file);
     console.log('The server is running on port ', port);
+  }
+
+  httpListen(port) {
+    const { app } = this;
+    http.createServer(app).listen(port, () => console.log('The HTTP server is running on port ', port));
+  }
+
+  httpsListen(port) {
+    const { app } = this;
+    https.createServer(app).listen(port, () => console.log('The HTTP server is running on port ', port));
   }
 }
